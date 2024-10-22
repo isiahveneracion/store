37523a00996afc073b0a09aa27c70a823076ae70
igbveneracion <igbveneracion@addu.edu.ph>
2024-10-22 19:43:20 +0800

    PRODUCT

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Button from "../Button";
+import { Link } from "react-router-dom";
 
-function ProductCard({ imageSrc, name }) {
+function ProductCard({ imageSrc, name, id }) {
   return (
     <div className="border shadow-md flex flex-col items-center gap-2 w-40 px-4 py-2">
       <img src={imageSrc} className="w-24 h-[124]" />
       <span className="font-bold text-sm">{name}</span>
 
-      <div className="flex justify-end"></div>
-      <Button
-        name="View"
-        onClick={() => console.log(name)}
-        className="bg-green-500 hover:bg-green-500/70"
-      />
+      <div>
+        <Link
+          to={`/${id}`}
+          className="bg-green-500 hover:bg-green-500/70 px-4 py-2 rounded-lg"
+        >
+          View
+        </Link>
+      </div>
     </div>
   );
 }
